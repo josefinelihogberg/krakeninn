d948a6e67711931c1d4c9fb1a970dd60e13da248
Victoria Esko <eskovictoria@gmail.com>
2022-12-16 16:36:10 +0100

    added translate, english and swedish

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 
 
+// translate
 
- // function that change the lang from swe to eng and the other way
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-      { pageLanguage: 'en',
-        includedLanguages: 'sv,en',
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-      },
-      'google_translate_element'
-  );
+  new google.translate.TranslateElement({pageLanguage: "en"}, 'google_translate_element');
+}
+
+function ChangeSwe(e) {
+  var lang = document.getElementById("lang-sv").value;
+  var selectField = document.querySelector("#google_translate_element select");
+  for(var i=0; i < selectField.children.length; i++){
+    var option = selectField.children[i];
+    // find desired langauge and change the former language of the hidden selection-field 
+    if(option.value==lang){
+       selectField.selectedIndex = i;
+       // trigger change event afterwards to make google-lib translate this side
+       selectField.dispatchEvent(new Event('change'));
+       break;
+    }
+  }
+}
+
+function changeEng(e) {
+  var lang = document.getElementById("lang-en").value;
+  var selectField = document.querySelector("#google_translate_element select");
+  for(var i=0; i < selectField.children.length; i++){
+    var option = selectField.children[i];
+    // find desired langauge and change the former language of the hidden selection-field 
+    if(option.value==lang){
+       selectField.selectedIndex = i;
+       // trigger change event afterwards to make google-lib translate this side
+       selectField.dispatchEvent(new Event('change'));
+       break;
+    }
+  }
 }
