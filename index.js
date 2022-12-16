693a5e820a9f46b43882441e7ad8754a4be7d2d8
Anna Krifors <krifors@hotmail.com>
2022-12-16 13:18:13 +0100

    a input for spendingmoney

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const p = document.querySelector('.test')
 
-
+let counter = 0;
 
  // function that change the lang from swe to eng and the other way
 function googleTranslateElementInit() {
@@ -11,3 +12,40 @@ function googleTranslateElementInit() {
       'google_translate_element'
   );
 }
+createFormMoneyInput()
+function createFormMoneyInput(){
+  let moneyForm = document.createElement('form');
+  let moneyAmountInput = document.createElement('input');
+  let moneyAmountSubmitBtn = document.createElement('button');
+  moneyAmountSubmitBtn.innerText = 'add my money';
+ moneyAmountInput.setAttribute('type', 'text')
+  p.append(moneyForm);
+  moneyForm.append(moneyAmountInput);
+  moneyForm.append(moneyAmountSubmitBtn);
+  moneyForm.addEventListener('submit', function(event){
+    event.preventDefault();
+  let moneyAmountP = document.createElement('p');
+  p.append(moneyAmountP);
+  counter = moneyAmountInput.value;
+  moneyAmountP.innerText = counter;
+  })
+}
+
+
+
+
+
+
+
+
+/*
+function addCounter(event){
+  event.preventDefault();
+  let moneyAmountP = document.createElement('p');
+  p.append(moneyAmountP);
+  counter = moneyAmountInput.value;
+  moneyAmountP.innerText = counter;
+
+}
+
+*/
