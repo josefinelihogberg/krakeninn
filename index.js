e3f1fc825740369c93a58f856671582ec5a11ac4
Anna Krifors <krifors@hotmail.com>
2022-12-16 16:24:05 +0100

    functions for counter

--- index.js
+++ index.js
@@ -24,17 +24,72 @@ function createFormMoneyInput(){
   moneyForm.append(moneyAmountSubmitBtn);
   moneyForm.addEventListener('submit', function(event){
     event.preventDefault();
-  let moneyAmountP = document.createElement('p');
-  p.append(moneyAmountP);
-  counter = moneyAmountInput.value;
-  moneyAmountP.innerText = counter;
+    let moneyAmountP = document.createElement('p');
+    p.append(moneyAmountP);
+    counter = moneyAmountInput.value;
+    moneyAmountP.innerText = `Ditt saldo är: ${counter} kr`;
+  
   })
+}
+
+
+
+/*
+  function getPriceOfCart(arr){
+    let newCounterArr = arr.map((element)=>{
+      return arr.element.price;
+    });
+    return newCounterArr;
+
+}
+
+*/
+
+const testList = [ {
+  "id": "ribs-brisket-and-burnt-ends",
+  "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/79619/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+  "name": "Joe's KC BBQ",
+  "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+  "price": 110.99,
+  "rate": 4,
+  "country": "Kansas City, KS"
+},
+{
+  "id": "005-kings-carolina-oink-sampler",
+  "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/66752/carolina-bbq-oink-sampler.1340b5a10cedc238cb2280306dd1d5a5.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+  "name": "Kings BBQ",
+  "dsc": "Carolina BBQ Oink Sampler",
+  "price": 89,
+  "rate": 4,
+  "country": "Kinston, NC"
+}]
+    
+function getTotalPrice(arr) {
+  counter = p.innerText;
+  let newCounterArr = arr.map(function(element){
+    return  `${element.price}`;
+  })
+  counter = newCounterArr.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+});
+return counter;
+  
 }
 
 
 
 
 
+
+
+
+/*
+function ListEmails(arr) {
+  let mailList = users.map((element) => {
+    return arr.element.email;
+  });
+  return mailList;
+}
 
 
 
